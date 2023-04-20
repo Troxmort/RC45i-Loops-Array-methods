7c64c6001cb511cc10cc95eb7bc239b57ec7356d
Homet Matias <mhometherrera@gmail.com>
2023-04-19 21:29:20 -0300

    Se avanzo en la clase viendo el bucle while

--- js/loops/while.js
+++ js/loops/while.js
@@ -1,1 +1,8 @@
-console.log("Bucle While");
+const numTabla = parseInt(prompt("Ingrese un numero para generar la tabla de multiplicar"))
+let i = 1;
+
+while (i <= 10) {
+    const multiplicacion = i * numTabla;
+    console.log(`El resultado de ${i}x${numTabla} = ${multiplicacion}`);
+    i++;
+}
