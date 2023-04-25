5a4876c49016895dc782717c8eb16255f7b4f2a2
Homet Matias <mhometherrera@gmail.com>
2023-04-24 21:41:00 -0300

    Se realizaron ejercicios del for

---
+++ js/loops/for-array.js
@@ -0,0 +1,28 @@
+const paisesLatam = [
+    "Argentina",
+    "Bolivia",
+    "Brasil",
+    "Chile",
+    "Colombia",
+    "Ecuador",
+    "Guyana",
+    "Guyana Francesa",
+    "Paraguay",
+    "Perú",
+    "Suriname",
+    "Uruguay",
+    "Venezuela",
+    "Guyana",
+    "Guyana Francesa",
+    "Paraguay",
+    "Perú",
+    "Suriname",
+    "Uruguay",
+    "Venezuela",
+];
+
+for (let i = 0; i < paisesLatam.length; i++) {
+
+    document.write(`<p>${i} - ${paisesLatam[i]}</p>`);
+
+}
--- js/loops/for.js
+++ js/loops/for.js
@@ -1,7 +1,6 @@
 const potencia = prompt("Ingrese el numero de la potencia");
 
-for (let i = 0; i <= 20; i++) {
+for (let i = 1; i <= 20; i++) {
     const resultado = Math.pow(i, potencia);
     document.write(`${i} ^ ${potencia} = ${resultado} <br>`);
-    
 }
