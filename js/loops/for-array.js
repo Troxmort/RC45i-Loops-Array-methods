7522424c70c83cdbe2f1310b2962bd75b3458208
Homet Matias <mhometherrera@gmail.com>
2023-05-27 17:00:09 -0300

    Se agrego bucles en for array

--- js/loops/for-array.js
+++ js/loops/for-array.js
@@ -23,6 +23,15 @@ const paisesLatam = [
 
 for (let i = 0; i < paisesLatam.length; i++) {
 
-    document.write(`<p>${i} - ${paisesLatam[i]}</p>`);
+    const numero = (paisesLatam.length - i);
+    // Forma 1 de poner un cero adelante
+    const numeroToString = String(numero);
+    const numeroTwoDigits = numeroToString.padStart(2, '0');
+
+    //Forma 2 de poner un 0 adelante
+
+    // if (numero < 10) numero = '0' + numero;
+
+    document.write(`<p>${numeroTwoDigits} - ${paisesLatam[i]}</p>`);
 
 }
