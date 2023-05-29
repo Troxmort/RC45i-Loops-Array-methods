8c65b2353d5dfc32f41b598b5a3eda1aded7ba0d
Homet Matias <mhometherrera@gmail.com>
2023-05-29 16:12:08 -0300

    Se agrego el eliminar los paises y el sumar los habitantes

--- js/arrays/methods.js
+++ js/arrays/methods.js
@@ -1,95 +1,211 @@
 const paisesLatinoamerica = [
   {
     nombre: "Argentina",
-    ubicacion: "Sur de Sudamérica",
-    habitantes: 45000000,
     capital: "Buenos Aires",
+    habitantes: 44938712,
+    ubicacion: "Sudamérica"
   },
   {
     nombre: "Bolivia",
-    ubicacion: "Centro de Sudamérica",
-    habitantes: 11000000,
     capital: "La Paz",
+    habitantes: 11469896,
+    ubicacion: "Sudamérica"
   },
   {
     nombre: "Brasil",
-    ubicacion: "Este de Sudamérica",
-    habitantes: 211000000,
     capital: "Brasilia",
+    habitantes: 211049527,
+    ubicacion: "Sudamérica"
   },
   {
     nombre: "Chile",
-    ubicacion: "Sur de Sudamérica",
-    habitantes: 19000000,
     capital: "Santiago",
+    habitantes: 18054726,
+    ubicacion: "Sudamérica"
   },
   {
     nombre: "Colombia",
-    ubicacion: "Norte de Sudamérica",
-    habitantes: 50340000,
     capital: "Bogotá",
+    habitantes: 50372424,
+    ubicacion: "Sudamérica"
   },
   {
     nombre: "Costa Rica",
-    ubicacion: "Centroamérica",
-    habitantes: 5100000,
     capital: "San José",
+    habitantes: 5047561,
+    ubicacion: "Centroamérica"
   },
   {
     nombre: "Cuba",
-    ubicacion: "El Caribe",
-    habitantes: 11400000,
     capital: "La Habana",
+    habitantes: 11209628,
+    ubicacion: "Caribe"
   },
   {
     nombre: "Ecuador",
-    ubicacion: "Oeste de Sudamérica",
-    habitantes: 17300000,
     capital: "Quito",
+    habitantes: 17373662,
+    ubicacion: "Sudamérica"
   },
   {
     nombre: "El Salvador",
-    ubicacion: "Centroamérica",
-    habitantes: 6500000,
     capital: "San Salvador",
+    habitantes: 6704864,
+    ubicacion: "Centroamérica"
   },
   {
     nombre: "Guatemala",
-    ubicacion: "Centroamérica",
-    habitantes: 18200000,
     capital: "Ciudad de Guatemala",
+    habitantes: 17679735,
+    ubicacion: "Centroamérica"
   },
   {
     nombre: "Honduras",
-    ubicacion: "Centroamérica",
-    habitantes: 10000000,
     capital: "Tegucigalpa",
+    habitantes: 9746117,
+    ubicacion: "Centroamérica"
   },
   {
     nombre: "México",
-    ubicacion: "Norte de América Central",
-    habitantes: 128900000,
     capital: "Ciudad de México",
+    habitantes: 126190788,
+    ubicacion: "Norteamérica"
   },
   {
     nombre: "Nicaragua",
-    ubicacion: "Centroamérica",
-    habitantes: 6400000,
     capital: "Managua",
+    habitantes: 6624554,
+    ubicacion: "Centroamérica"
   },
   {
     nombre: "Panamá",
-    ubicacion: "Centroamérica",
-    habitantes: 4300000,
     capital: "Ciudad de Panamá",
+    habitantes: 4246439,
+    ubicacion: "Centroamérica"
   },
   {
     nombre: "Paraguay",
-    ubicacion: "Centro de Sudamérica",
-    habitantes: 7200000,
     capital: "Asunción",
+    habitantes: 7152703,
+    ubicacion: "Sudamérica"
   },
-];
+  {
+    nombre: "Perú",
+    capital: "Lima",
+    habitantes: 32165485,
+    ubicacion: "Sudamérica"
+  },
+  {
+    nombre: "República Dominicana",
+    capital: "Santo Domingo",
+    habitantes: 10766998,
+    ubicacion: "Caribe"
+  },
+  {
+    nombre: "Uruguay",
+    capital: "Montevideo",
+    habitantes: 3473730,
+    ubicacion: "Sudamérica"
+  },
+  {
+    nombre: "Venezuela",
+    capital: "Caracas",
+    habitantes: 28515829,
+    ubicación: "Sudamérica"
+  },
+  {
+  nombre: "Puerto Rico",
+  capital: "San Juan",
+  habitantes: 2860853,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "Guayana Francesa",
+  capital: "Cayena",
+  habitantes: 294071,
+  ubicacion: "Sudamérica"
+  },
+  {
+  nombre: "Surinam",
+  capital: "Paramaribo",
+  habitantes: 581372,
+  ubicacion: "Sudamérica"
+  },
+  {
+  nombre: "Guyana",
+  capital: "Georgetown",
+  habitantes: 782766,
+  ubicacion: "Sudamérica"
+  },
+  {
+  nombre: "Haití",
+  capital: "Puerto Príncipe",
+  habitantes: 11577779,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "Jamaica",
+  capital: "Kingston",
+  habitantes: 2948279,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "Trinidad y Tobago",
+  capital: "Puerto España",
+  habitantes: 1394973,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "Bahamas",
+  capital: "Nasáu",
+  habitantes: 385340,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "Barbados",
+  capital: "Bridgetown",
+  habitantes: 287025,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "Santa Lucía",
+  capital: "Castries",
+  habitantes: 183645,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "San Cristóbal y Nieves",
+  capital: "Basseterre",
+  habitantes: 52441,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "San Vicente y las Granadinas",
+  capital: "Kingstown",
+  habitantes: 110608,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "Antigua y Barbuda",
+  capital: "Saint John's",
+  habitantes: 104084,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "Dominica",
+  capital: "Roseau",
+  habitantes: 71986,
+  ubicacion: "Caribe"
+  },
+  {
+  nombre: "Granada",
+  capital: "Saint George's",
+  habitantes: 112003,
+  ubicacion: "Caribe"
+  }
+  ];
+
+const copiaArray = Array.from(paisesLatinoamerica);
 
 const tableBodyHtml = document.getElementById('tableBody');
 
@@ -112,6 +228,29 @@ function aplicarFiltroNombre(evtDesdeHTML) {
 
 }
 
+function borrarPais(indice) {
+
+  copiaArray.splice(indice, 1); 
+
+  renderizarTable(copiaArray);
+  calcularPoblacionTotal(copiaArray);
+
+}
+
+function calcularPoblacionTotal(listaPaises) {
+
+  const acumuladorTotal = listaPaises.reduce((acumulador, pais) => {
+    
+    return acumulador += pais.habitantes;
+
+  }, 0);
+
+  console.log(`Acumulador: ${acumuladorTotal}`);
+
+}
+
+calcularPoblacionTotal(paisesLatinoamerica);
+
 function renderizarTable(arrayDePaises) {
 
   tableBodyHtml.innerHTML = '';
@@ -127,6 +266,9 @@ function renderizarTable(arrayDePaises) {
     <td>${pais.capital}</td>
     <td>${pais.habitantes}</td>
     <td>${pais.ubicacion} </td>
+    <td>
+      <button class="btn btn-danger" px-1" onclick="borrarPais(${index})"> <i class="fa-solid fa-trash"> </i></button>
+    </td>
     </tr>`;
 
   })
